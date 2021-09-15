Added "correct guesses" to evaluation. On first 10000 training datasets, we got Correct guesses: 5478 / 10000, 54.779999999999994%. Average (quadratic) error: 0.3012118967771091

src/execute.py
@@ -10,7 +10,7 @@ input_shape = (28, 28, 1)
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 # TODO remove (test purposes)
-(x_train, y_train), (x_test, y_test) = (x_train[0:5001], y_train[0:5001]), (x_test, y_test)
+(x_train, y_train), (x_test, y_test) = (x_train[0:10001], y_train[0:10001]), (x_test, y_test)
 # Scale images to the [0, 1] range
 x_train = x_train.astype("float32") / 128 - 1
 x_test = x_test.astype("float32") / 128 - 1
@@ -35,6 +35,8 @@ model = Sequential()
 
 ### set up data
 
+y_test_old = y_test
+
 n = y_train.size
 y_train_new = np.zeros((n, 10))
 for i in range(n):
@@ -49,7 +51,7 @@ y_test = y_test_new
 ### Train the model
 
 model.fit(x_train, y_train)
-model.evaluate(x_test, y_test)
+model.evaluate(x_test, y_test, y_test_old)
 
 ### Evaluate the model
 

src/sequential.py
@@ -108,18 +108,24 @@ class Sequential:
 
         return self.o_k
 
-    def evaluate(self, x_test, y_test):
+    def evaluate(self, x_test, y_test, y_test_old):
         (n, x1, x2) = x_test.shape
         (n1, y1) = y_test.shape
         assert n == n1
+        # This contains the quadratic error: 1/2 * sum (y_i - o_k)^2
         error_vector = np.empty(n)
+        # This contains the guesses
+        guess_vector = np.empty(n)
         for i in range(n):
-            o_i = self.predict(x_test[i])
-            diff = y_test[i] - o_i
+            o_k = self.predict(x_test[i])
+            diff = y_test[i] - o_k
             error_vector[i] = 1 / 2 * np.dot(diff, diff)
+            guess_vector[i] = (np.argmax(o_k) == y_test_old[i])
+        correct_guesses = np.count_nonzero(guess_vector)
 
         print('Evaluation:')
         print('Test dataset: ' + str(n))
+        print('Correct guesses: ' + str(correct_guesses) + ' / ' + str(n) + ', ' + str(correct_guesses/n*100) + '%.')
         print('Average error: ' + str(np.average(error_vector)))
         print('Maximum error: ' + str(np.max(error_vector)))
         print('Minimum error: ' + str(np.min(error_vector)))